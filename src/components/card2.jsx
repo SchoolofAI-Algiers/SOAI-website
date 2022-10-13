625e695eb92886979d763a12ca034bcab7c26dcc
yassin <y.attuo@esi-sba.dz>
2022-10-13 16:43:19 +0200

    update

--- src/components/card2.jsx
+++ src/components/card2.jsx
@@ -5,7 +5,7 @@ export default function Card({title ,src,text}) {
   return (
     
     
-    <div className='shadow-lg mt-12 shadow-eventBg      border bottom-2 rounded-md  border-eventBg flex flex-col  items-center w-9/12 md:w-full mx-auto md:mx-2 
+    <div className='shadow-lg mt-12 shadow-eventBg mb-8   border bottom-2 rounded-md  border-eventBg flex flex-col  items-center w-9/12 md:w-full mx-auto md:mx-2 
     transition duration-200 ease-in transform sm:hover:scale-105'>
        <div className='bg-eventBg  w-3/5 mt-0 flex justify-center'>
            <h2 className='text-white text-lg'>{title}</h2>
