fd09a09098ae52b9665b8664e8706124e043b5f6
moonayyur <jm_kias@esi.dz>
2022-10-15 11:03:56 +0100

    Make navbar links a const

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,10 @@
 import "/src/styles.css";
-import Navbar from "../components/NavBar";
-
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-    {/* <Navbar />  */}
-    <Component {...pageProps} />
-    </>);
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,18 @@
-import { useState } from "react";
-import Greetings from "../sections/Greetings.jsx";
 import Head from "next/head";
 import Navbar from "../components/Navbar.jsx";
 function App() {
-
   return (
     <>
       <Head>
         <title>SOAI website</title>
-
       </Head>
-      {/* <div class="h-72 from-[#180d5b] to-white"> */}
-
-      <div className="bg-[#180d5b]  justify-around items-center min-h-screen text-white">
-           
-            <Navbar /> 
-            <h1 className="text-3xl font-bold underline">
-           </h1>
-       </div> 
+      {/* TO DO: replace background
+      bg color just to see the Navbar */}
+      <div className="bg-[#6a6491] justify-around items-center min-h-screen text-white">
+        <Navbar />
+        <h1 className="text-3xl font-bold underline"></h1>
+      </div>
     </>
   );
 }
-
 export default App;
