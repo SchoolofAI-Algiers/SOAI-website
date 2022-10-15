40d3dd468ec5be4aad71895cc526cda89567dd18
meryeemdifangel <83986365+meryeemdifangel@users.noreply.github.com>
2022-10-15 13:08:53 +0100

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,5 +1,5 @@
 export default function Hero() {
-    return   <div class="h-screen w-full bg-[url('/src/assets/hero.svg')] bg-cover bg-center">
+    return   <div class="h-screen w-full bg-[url('/src/hero.svg')] bg-cover bg-center">
     <div class="w-1/2 h-full flex flex-col items-start justify-center pl-5 " >
 <h1 class="font-bold text-2xl mb-1.5  ">SCHOOL OF AI ALGIERS</h1>
 <p class="font-semibold w-3/4 text-lg text-black/54">School of Algiers is a local community of developers, students and lovers of artificial intelligence in Algeria and we are 
