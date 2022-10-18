1fb5368954ce1618a11c576b325f27aa130569ab
meryeemdifangel <83986365+meryeemdifangel@users.noreply.github.com>
2022-10-18 20:01:31 +0100

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,17 +1,18 @@
-export default function Hero() {
+
+
+function Hero() {
   return (
-    <div className="h-screen w-full bg-[url('/hero.svg')] bg-cover bg-center">
-      <div className="w-1/2 h-full flex flex-col items-start justify-center pl-5 ">
-        <h1 className="font-bold text-2xl mb-1.5  ">SCHOOL OF AI ALGIERS</h1>
-        <p className="font-semibold w-3/4 text-lg text-black/54">
-          School of Algiers is a local community of developers, students and
-          lovers of artificial intelligence in Algeria and we are a specific
-          club at the higher national school of computer science (ESI
-          Algiers).We organize workshops like AI Day , hackathons like
-          Haick,international conferences like NeurIPS Algiers Meetup 2021 and
-          many other amazing events.
-        </p>
-      </div>
+  <div class="h-screen w-full   md:bg-[url('/src/assets/hero.svg')] bg-cover bg-center ">
+    <div class=" h-full w-full flex flex-col justify-center md:w-1/2 items-start pl-5 " >
+<h1 class="font-bold  mb-1.5  md:text-lg lg:text-2xl  ">SCHOOL OF AI ALGIERS</h1>
+<p class="font-semibold w-full  text-black/54 md:w-3/4  text-sm  lg:text-lg ">School of Algiers is a local community of developers, students and lovers of artificial intelligence in Algeria and we are 
+  a specific club at the higher national school of computer science (ESI Algiers).We organize workshops like AI Day , hackathons like Haick,international
+  conferences like NeurIPS Algiers Meetup 2021 and many other amazing events.
+</p>
     </div>
-  );
+    </div>
+    );
 }
+
+export default Hero;
+
