bf8e6d4e73d2ac6897dd64accbd4afe383e92378
moonayyur <jm_kias@esi.dz>
2022-10-24 22:47:04 +0100

    Fix brain animation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,11 +2,8 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="lg:h-screen py-52 -mt-14 w-full lg:py-0 lg:-mt-0 bg-blue lg:bg-[url('/hero-bg.svg')] flex flex-col bg-cover bg-center">
-      <div className="absolute animate-brain-move hidden lg:block top-1/3 w-40 xl:w-auto right-[15%] xl:right-72">
-        <Image src="/brain.svg" alt="" width={200} height={200} />
-      </div>
-      <div className="lg:w-2/5 h-full gap-y-10 flex flex-col items-center justify-center px-8 sm:px-16 lg:pr-10 lg:pl-16 lg:items-start">
+    <div className="lg:flex justify-between lg:h-screen py-52 -mt-14 w-full lg:py-0 lg:-mt-0 bg-blue lg:bg-[url('/empty-bg.svg')] bg-cover bg-center">
+      <div className="lg:w-5/12 lg:h-screen gap-y-10 flex flex-col items-center justify-center px-8 sm:px-16 lg:pr-10 lg:pl-16 lg:items-start">
         <div className="font-extrabold text-4xl w-full text-start sm:text-5xl text-white lg:text-blue">
           SCHOOL OF AI ALGIERS
         </div>
@@ -18,6 +15,20 @@ export default function Hero() {
           international conferences like NeurIPS Algiers Meetup 2021 and many
           other amazing events.
         </p>
+      </div>
+      {/* Elements */}
+      <div className="lg:flex flex-col hidden items-center absolute bottom-0 right-0 -space-y-60 justify-start">
+        <img
+          src="/brain-light.png"
+          alt=""
+          className="w-[200px] z-10 mb-1 ml-[300px] "
+        />
+        <img
+          src="/brain.svg"
+          alt=""
+          className="w-[150px] ml-72 animate-brain-move z-20"
+        />
+        <img src="/empty-main.svg" alt="" className="w-[900px]" />
       </div>
       {/* Mobile image */}
       <div className="lg:hidden h-full w-full flex flex-col items-center mt-10 sm:mt-20 -mb-40 -space-y-40 sm:-space-y-48">
