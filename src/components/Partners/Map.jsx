b8d9cb8d2bc86bff3d56e4881aa4bfdc6078c666
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-10-27 19:57:43 +0100

    fix: partners map

--- src/components/Partners/Map.jsx
+++ src/components/Partners/Map.jsx
@@ -1,14 +1,10 @@
-import Image from "next/image";
-
 export default function Map() {
   return (
     <div className="hidden sm:block relative xl:h-[500px] xl:w-[1200px] lg:h-[450px] lg:w-[900px] md:h-[400px] md:w-[750px] sm:h-[300px] sm:w-[600px]">
-      <Image
-        layout="fill"
-        objectFit="contain"
+      <img
         src="/Globe.png"
         alt=""
-        className="md:opacity-80"
+        className="md:opacity-80 absolute object-fill"
       />
     </div>
   );
